7ba6ede059b549fccf65296fae13d4270102765e
WilkP <s13705@pjwstk.edu.pl>
2017-03-06 07:29:42 +0100

    Extended functional test

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -13,7 +14,24 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get("http://localhost:8000")
 
         self.assertIn('Listy', self.browser.title)
-        self.fail('Zakończenie testu!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('Listy', header_text)
 
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Wpisz rzecz do zrobienia')
+
+        inputbox.send_keys('Kupić pawie pióra')
+
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+
+        self.assertTrue(any(row.text == '1: Kupić pawie pióra' for row in rows)
+                        )
+
+        self.fail('Zakończenie testu!')
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
