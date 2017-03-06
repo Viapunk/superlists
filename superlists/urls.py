50b06206b08b8882d1dfe1c94de4f2a68a6342c4
WilkP <s13705@pjwstk.edu.pl>
2017-03-06 06:27:54 +0100

    First unit test, mapping url and dummy view

--- superlists/urls.py
+++ superlists/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from lists import views as lists_views
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', lists_views.home_page, name='home'),
     url(r'^admin/', admin.site.urls),
 ]
