3b04a1793b8b554f4e17c49b6fa549fdc4b201fb
WilkP <s13705@pjwstk.edu.pl>
2017-03-06 06:53:02 +0100

    Simple view generates minimal amount of HTML

--- superlists/wsgi.py
+++ superlists/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "superlists.settings")
 
 application = get_wsgi_application()
+path = '/home/vpnk/workspace/python/superlists'
